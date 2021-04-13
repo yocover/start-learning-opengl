95170cde3a7d6dcb914fb28092a5e8e88f0bfd8c
yocover <yocover@163.com>
2021-04-13 22:14:22 +0800

    update

---
+++ src/02_hello_triangle/main.cpp
@@ -0,0 +1,188 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+
+const char *vertexShaderSource = "#version 330 core\n"
+                                 "layout (location = 0) in vec3 aPos;\n"
+                                 "void main()\n"
+                                 "{\n"
+                                 "gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);"
+                                 "gl_PointSize = 20.0f;"
+                                 "}\n";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+                                   "out vec4 FragColor;\n"
+                                   "void main()\n"
+                                   "{"
+                                   "FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);"
+                                   "}";
+
+int main()
+{
+  glfwInit();
+  // 设置主要和次要版本
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // 创建窗口对象
+  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL)
+  {
+    std::cout << "Failed to create GLFW window" << std::endl;
+    glfwTerminate();
+    return -1;
+  }
+  glfwMakeContextCurrent(window);
+
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+  {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+  // 设置视口
+  glViewport(0, 0, 800, 600);
+  glEnable(GL_PROGRAM_POINT_SIZE);
+
+  // 注册窗口变化监听
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // 定义顶点数组
+  float vertices[] = {
+      0.5f, 0.5f, 0.0f,   // 右上角
+      0.5f, -0.5f, 0.0f,  // 右下角
+      -0.5f, -0.5f, 0.0f, // 左下角
+      -0.5f, 0.5f, 0.0f   // 左上角
+  };
+  unsigned int indexes[] = {
+      0, 1, 3, // 第一个三角形
+      1, 2, 3  // 第二个三角形
+  };
+
+  // 创建缓冲对象
+  unsigned int VBO, VAO, EBO;
+  glGenVertexArrays(1, &VAO);
+  glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
+
+  // 绑定VAO缓冲对象
+  glBindVertexArray(VAO);
+
+  // 绑定VBO缓对象
+  glBindBuffer(GL_ARRAY_BUFFER, VBO);
+  // 填充VBO数据
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // 绑定EBO对象
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  // 填充EBO数据
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexes), indexes, GL_STATIC_DRAW);
+
+  // 设置顶点属性指针
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+  glEnableVertexAttribArray(0);
+
+  glBindVertexArray(0);
+
+  // 创建顶点和片段着色器
+  unsigned int vertexShader, fragmentShader;
+  vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+  // 附加着色器代码
+  glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+  glCompileShader(vertexShader);
+
+  glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+  glCompileShader(fragmentShader);
+
+  // 检测是否编译成功
+  int success;
+  char infoLog[512];
+  glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+  if (!success)
+  {
+    glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+    std::cout << "ERROR:SHADER::VERTEX::COMPILATION_FAILED\n"
+              << std::endl;
+  }
+
+  glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+  if (!success)
+  {
+    glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+    std::cout << "ERROR:SHADER::FRAGMENT::COMPILATION_FAILED\n"
+              << std::endl;
+  }
+
+  // 创建程序对象
+  unsigned int shaderProgram;
+  shaderProgram = glCreateProgram();
+  // 将着色器附加到程序对象上
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+
+  // 链接
+  glLinkProgram(shaderProgram);
+  glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+  if (!success)
+  {
+    glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+    std::cout << "ERROR:SHADER_PROGRAM_LINK_FAILED" << std::endl;
+  }
+
+  // 使用着色器程序
+  glUseProgram(shaderProgram);
+
+  // 删除着色器
+  glDeleteShader(vertexShader);
+  glDeleteShader(fragmentShader);
+
+  // 设置线框绘制模式
+  glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+
+  while (!glfwWindowShouldClose(window))
+  {
+    processInput(window);
+
+    // 渲染指令
+    // ...
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glUseProgram(shaderProgram);
+    glBindVertexArray(VAO);
+    // glDrawArrays(GL_POINTS, 0, 6);
+    // glDrawArrays(GL_LINE_LOOP, 0, 6);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    glBindVertexArray(0);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glDeleteVertexArrays(1, &VAO);
+  glDeleteBuffers(1, &VBO);
+  glDeleteBuffers(1, &EBO);
+  glDeleteProgram(shaderProgram);
+
+  glfwTerminate();
+  return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+  glViewport(0, 0, 800, 600);
+}
+
+void processInput(GLFWwindow *window)
+{
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+  {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
