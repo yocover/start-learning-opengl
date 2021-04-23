ed05d2a1b2b0070cd86f5613db82d7c80b1dccb8
yocover <yocover@163.com>
2021-04-23 21:35:24 +0800

    update

--- src/07_load_texture/shader/fragment.glsl
+++ src/07_load_texture/shader/fragment.glsl
@@ -7,5 +7,6 @@ uniform sampler2D texture1;
 uniform sampler2D texture2;
 
 void main() {
-  FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+  // FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
+  FragColor = texture(texture1, TexCoord);
 }
--- src/07_load_texture/shader/vertex.glsl
+++ src/07_load_texture/shader/vertex.glsl
@@ -9,5 +9,5 @@ out vec2 TexCoord;
 void main() {
   gl_Position = vec4(aPos, 1.0f);
   ourColor = aColor;
-  TexCoord = aTexCoord;
+  TexCoord = aTexCoord * 2.0;
 }
