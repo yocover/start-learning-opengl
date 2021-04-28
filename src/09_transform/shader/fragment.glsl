1af5683cec0efc72b3ec1afab78fa77116ac6a17
yocover <yocover@163.com>
2021-04-28 22:06:51 +0800

    update

--- src/09_transform/shader/fragment.glsl
+++ src/09_transform/shader/fragment.glsl
@@ -7,22 +7,7 @@ in vec2 TexCoord;
 uniform sampler2D texture1;
 uniform sampler2D texture2;
 
-/**
-  * |  cos0  -sin0   0   0|    |x|    |x * cos0 - y * sin0|
-  * |  sin0   cos0   0   0|  * |y| =  |x * sin0 + y * cos0|
-  * |  0       0     1   0|    |z|    |         z         |
-  * |  0       0     0   1|    |w|    |         1         |
-  * 可以用矩阵来旋转坐标系统
-  */
-mat4 rotate3d(float _angle) {
-  return mat4(cos(_angle), -sin(_angle), 0, 0 sin(_angle), cos(_angle), 0, 0 0, 0, 1, 0, 0, 0, 0, 1);
-}
-
-uniform float factor;
-
 void main() {
-  // FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
-
   float xy = length(ourPos.xy);
-  FragColor = rotate3d(45.0) * mix(texture(texture1, TexCoord), vec4(ourColor, 1.0 - xy * 2.0) * texture(texture2, TexCoord), 0.1);
+  FragColor = mix(texture(texture1, TexCoord), vec4(ourColor, 1.0 - xy * 2.0) * texture(texture2, TexCoord), 0.1);
 }
--- src/09_transform/shader/vertex.glsl
+++ src/09_transform/shader/vertex.glsl
@@ -7,9 +7,23 @@ out vec3 ourColor;
 out vec3 ourPos;
 out vec2 TexCoord;
 
+uniform float factor;
+
+/**
+  * |  cos0  -sin0   0   0|    |x|    |x * cos0 - y * sin0|
+  * |  sin0   cos0   0   0|  * |y| =  |x * sin0 + y * cos0|
+  * |  0       0     1   0|    |z|    |         z         |
+  * |  0       0     0   1|    |w|    |         1         |
+  * 可以用矩阵来旋转坐标系统
+  */
+mat4 rotate3d(float _angle) {
+  return mat4(cos(_angle), -sin(_angle), 0.0f, 0.0f, sin(_angle), cos(_angle), 0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, 1.0f);
+}
+
 void main() {
-  gl_Position = vec4(aPos, 1.0f);
+  gl_Position = vec4(rotate3d(factor) * vec4(aPos, 1.0f));
   gl_PointSize = 10.0f;
+
   ourColor = aColor;
   ourPos = aPos;
   TexCoord = aTexCoord * 2.0;
