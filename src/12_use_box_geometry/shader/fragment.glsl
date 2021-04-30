51f0e497d3e7d1a8d1ecd3e795f501ca713930c3
yocover <yocover@163.com>
2021-04-30 16:24:46 +0800

    update

--- src/12_use_box_geometry/shader/fragment.glsl
+++ src/12_use_box_geometry/shader/fragment.glsl
@@ -7,9 +7,9 @@ uniform sampler2D texture1;
 uniform sampler2D texture2;
 
 void main() {
-  // FragColor = mix(texture(texture1, outTexCoord), texture(texture2, outTexCoord), 0.1);
+  FragColor = mix(texture(texture1, outTexCoord), texture(texture2, outTexCoord), 0.1);
 
-  vec2 coord = gl_PointCoord - vec2(0.5);
-  float r = float(length(coord) > 0.5);
-  FragColor = vec4(0.0, stp * 0.5, stp, (1 - r) * stp);// 蓝色过度色
+  // vec2 coord = gl_PointCoord - vec2(0.5);
+  // float r = float(length(coord) > 0.5);
+  // FragColor = vec4(0.0, stp * 0.5, stp, (1 - r) * stp);// 蓝色过度色
 }
