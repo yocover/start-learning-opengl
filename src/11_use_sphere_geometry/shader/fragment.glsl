07b37ecf8f7fb7f92cc18b9ca89ad6474b2c1a6f
yocover <yocover@163.com>
2021-04-30 15:30:32 +0800

    update

--- src/11_use_sphere_geometry/shader/fragment.glsl
+++ src/11_use_sphere_geometry/shader/fragment.glsl
@@ -7,10 +7,10 @@ uniform sampler2D texture2;
 
 void main() {
   // FragColor = mix(texture(texture1, outTexCoord), texture(texture2, outTexCoord), 0.1);
-  FragColor = vec4(1.0, 0.6, 0.1, 0.3);
+  // FragColor = vec4(1.0, 0.6, 0.1, 0.3);
 
-  // vec2 coord = gl_PointCoord - vec2(0.5);
+  vec2 coord = gl_PointCoord - vec2(0.5);
 
-  // float r = float(length(coord) > 0.5);
-  // FragColor = vec4(0.2, 0.91, 0.4, (1 - r) * 0.1);
+  float r = float(length(coord) > 0.5);
+  FragColor = vec4(0.0, 0.91, 0.9, (1 - r) * 0.1);
 }
