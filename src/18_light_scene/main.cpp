37bf2f1853b7aea12b59d072f6216a2ca6f68d0a
yocover <yocover@163.com>
2021-08-14 07:38:40 +0800

    update

--- src/18_light_scene/main.cpp
+++ src/18_light_scene/main.cpp
@@ -52,7 +52,7 @@ int main(int argc, char *argv[])
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-  // 创建窗口对象
+  // 窗口对象
   GLFWwindow *window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "LearnOpenGL", NULL, NULL);
   if (window == NULL)
   {
