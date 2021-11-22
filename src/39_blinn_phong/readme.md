29179504c0df0e503d029b3f5c12a664fa288804
yocover <yocover@163.com>
2021-11-22 18:16:06 +0800

    update

---
+++ src/39_blinn_phong/readme.md
@@ -0,0 +1,19 @@
+## Blinn phong
+
+### reflect dir
+
+![image-20211122181430652](images/image-20211122181430652.png)
+
+![image-20211122181317436](images/image-20211122181317436.png)
+
+### halfway dir
+
+![image-20211122181441708](images/image-20211122181441708.png)
+
+![image-20211122181407818](images/image-20211122181407818.png)
+
+
+
+## 参考
+
+https://learnopengl-cn.github.io/05%20Advanced%20Lighting/01%20Advanced%20Lighting/#_1
---
+++ src/39_blinn_phong/shader/scene_frag.glsl
@@ -0,0 +1,45 @@
+#version 330 core
+out vec4 FragColor;
+
+uniform sampler2D woodMap; // 贴图
+
+in vec2 outTexCoord;
+in vec3 outNormal;
+in vec3 outFragPos;
+
+uniform vec3 viewPos;
+uniform vec3 lightPos;
+uniform vec3 lightColor;
+uniform float strength;
+
+uniform bool blinn;
+
+void main() {
+
+  vec3 color = texture(woodMap, outTexCoord).rgb;
+
+  vec3 ambient = strength * color; // 环境光
+
+  vec3 normal = normalize(outNormal);
+  vec3 lightDir = normalize(lightPos - outFragPos);
+
+  float diff = max(dot(normal, lightDir), 0.0);
+  vec3 diffuse = diff * color; // 漫反射
+
+  vec3 viewDir = normalize(viewPos - outFragPos);
+
+  float spec = 0.0;
+  if(blinn) {
+    vec3 halfwayDir = normalize(lightDir + viewDir);
+    spec = pow(max(dot(viewDir, halfwayDir), 0.0), 16.0);
+
+  } else {
+    vec3 reflectDir = reflect(-lightDir, normal);
+    spec = pow(max(dot(viewDir, reflectDir), 0.0), 8.0);
+  }
+
+  vec3 specular = vec3(0.3) * spec; // 镜面光
+  vec3 result = (ambient + diffuse + specular);
+
+  FragColor = vec4(result, 1.0);
+}
